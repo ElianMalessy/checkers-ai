5e6be18fddc6f462bfbac5f54c4d52f5f124e8fe
“Harikrishna <hvardhin@uci.edu>
2025-03-01 20:18:17 -0800

    Re-implemented minimal AI code in python

--- Tools/run_tests.sh
+++ Tools/run_tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for i in {1..20}
+for i in {1..10}
 do
     echo "Starting test $i"
-    python3 AI_Runner.py 8 8 3 l ../src/checkers-cpp/main Sample_AIs/Random_AI/main.py > output/$i.txt
+    python3 AI_Runner.py 8 8 3 l ../src/checkers-python/main.py Sample_AIs/Random_AI/main.py > output/$i.txt
     echo "Finished test $i"
 done
--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -1,6 +1,109 @@
 from random import randint
 from BoardClasses import Move
 from BoardClasses import Board
+from Checker import Checker
+import math
+
+def copyBoard(oldboard):
+        newBoard = Board(oldboard.col, oldboard.row, oldboard.p)
+        newBoard.board = [[0 for _ in newBoard.col] for _ in newBoard.row]
+        for i in range(len(newBoard.board)):
+            for j in range(len(newBoard.board[0])):
+                newBoard.board[i][j] = Checker(oldboard.board[i][j].color, i, j)
+                newBoard.board[i][j].is_king = oldboard.board[i][j].is_king
+        newBoard.col = oldboard.col
+        newBoard.row = oldboard.row
+        newBoard.p = oldboard.p
+        newBoard.black_count = oldboard.black_count
+        newBoard.tie_counter = oldboard.tie_counter
+        return newBoard
+
+class MCTSNode:
+
+    def __init__(self, player, move=0, parent=None):
+        self.player = player
+        self.move = move
+        self.parent = parent
+        self.children = []
+
+        self.visits = 0
+        self.wins = 0
+
+    def UCTValue(self, constant):
+        if not visits: return float('inf')
+        return (wins/visits) + c * math.sqrt(math.log(self.parent.visits)/visits)
+    
+    def SelectChild(self):
+        bestValue = float('-inf')
+        bestChild = None
+
+        for child in children:
+            uct = child.UCTValue()
+            if (uct > bestValue):
+                bestValue = uct
+                bestChild = child
+        return bestChild
+    
+    def evaluateMaterial(self, board):
+        material = 0
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                piece = board[i][j]
+                if piece.is_king:
+                    if piece.color == 'W':
+                        material += 3
+                    elif piece.color == 'B':
+                        material -= 3
+                else:
+                    if piece.color == 'W':
+                        material += 1
+                    elif piece.color == 'B':
+                        material -= 1
+        return material
+    
+    def evaluateBoard(self, board):
+        return float(evaluateMaterial(board))
+    
+    
+
+    def bestMove(self, board, possible_moves):
+        bestMove = Move(possible_moves)
+        bestScore = 0
+
+        def keepBestScore(a, b):
+            if self.player == 1:
+                return max(a, b)
+            elif self.player == 2:
+                return min(a, b)
+
+        if self.player == 1:
+            bestScore = float('-inf')
+        elif self.player == 2:
+            bestScore = float('inf')
+        
+        newPlayer = 2 if player == 1 else 1
+
+        for move_set in possible_moves:
+            for m in move_set:
+                # Try the move
+                newBoard = copyBoard(board)
+                newBoard.make_move(m, player)
+
+                newNode = MCTSNode(newPlayer, m, self)
+                children.append(newNode)
+
+                # Apply heuristic
+                score = evaluateBoard(newBoard.board)
+                newBestScore = keepBestScore(bestScore, score)
+
+                if newBestScore != bestScore:
+                    bestScore = newBestScore
+                    bestMove = m
+        
+        return bestMove
+
+
+
 #The following part should be completed by students.
 #Students can modify anything except the class name and exisiting functions and varibles.
 class StudentAI():
@@ -14,6 +117,10 @@ class StudentAI():
         self.color = ''
         self.opponent = {1:2,2:1}
         self.color = 2
+
+        self.player = 2
+        self.MCTS_ITERATIONS = 50
+    
     def get_move(self,move):
         if len(move) != 0:
             self.board.make_move(move,self.opponent[self.color])
@@ -25,3 +132,67 @@ class StudentAI():
         move = moves[index][inner_index]
         self.board.make_move(move,self.color)
         return move
+
+
+
+    def RunMCTS(self):
+        root = MCTSNode
+
+        for i in range(MCTS_ITERATIONS):
+
+            # Select
+            node = root
+            newBoard = copyBoard(self.board)
+
+            while (node.visits > 0):
+                prevPlayer = node.player
+                next_node = node.SelectChild()
+                if not next_node:
+                    result = newBoard.is_win(prevPlayer)
+                    self.backpropagate(node, result)
+                    break
+                
+                node = next_node
+                newBoard.make_move(node.move, prevPlayer)
+
+            # Expand
+            possible_moves = newBoard.get_all_possible_moves(node.player)
+            winner = 0
+            if len(possible_moves) == 0:
+                winner = newBoard.is_win(2 if node.player == 1 else 1)
+            else:
+                bestMove = node.bestMove(newBoard, possible_moves)
+                newBoard.make_move(bestMove, node.player)
+
+                simPlayer = node.player
+                winner = self.rollout(newBoard, simPlayer)
+
+            # Backpropagate
+            self.backpropagate(node, winner)
+    
+    def rollout(self, board, curr_player):
+        while True:
+            winner = board.is_win(curr_player)
+            if winner:
+                return winner
+
+            moves = board.get_all_possible_moves(curr_player)
+            if not moves:
+                return 2 if curr_player == 1 else 1
+            
+            i = randint(0, len(moves)-1)
+            j = randint(0, len(moves[i])-1)
+            board.make_move(moves[i][j], curr_player)
+
+            curr_player = 2 if curr_player == 1 else 1
+
+
+    def backpropagate(self, node, result):
+        while node:
+            node.visits += 1
+
+            if winner == node.player or winner == -1:
+                node.wins += 1
+            else:
+                node.wins -= 1
+            node = node.parent
