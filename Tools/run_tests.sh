f6b30615ea6abfef336c0f1e08bac7cb162d55bd
“Harikrishna <hvardhin@uci.edu>
2025-03-01 20:36:27 -0800

    Fixed a bug with poor AI running into a broken pipe

--- Tools/run_tests.sh
+++ Tools/run_tests.sh
@@ -3,6 +3,6 @@
 for i in {1..10}
 do
     echo "Starting test $i"
-    python3 AI_Runner.py 8 8 3 l ../src/checkers-python/main.py Sample_AIs/Random_AI/main.py > output/$i.txt
+    python3 AI_Runner.py 8 8 3 l ../src/checkers-python/main.py Sample_AIs/Poor_AI/main.py > output/$i.txt
     echo "Finished test $i"
 done
