1efafab548c678b1ebcacb450ab1af594f4740fe
Elian Hijmans Malessy <elian.malessy@gmail.com>
2025-03-01 20:40:28 -0800

    stuff

--- Tools/run_tests.sh
+++ Tools/run_tests.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-for i in {1..10}
+mkdir output
+for i in {1..100}
 do
+    touch output/$i.txt
     echo "Starting test $i"
     python3 AI_Runner.py 8 8 3 l ../src/checkers-python/main.py Sample_AIs/Poor_AI/main.py > output/$i.txt
     echo "Finished test $i"
 done
+
+python3 parse_tests.py
